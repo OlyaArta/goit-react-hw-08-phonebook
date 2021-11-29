a514f9d33bc376fe93cac0b5e5490a5b760f07a1
OlyaArta <artanika8@gmail.com>
2021-11-30 00:53:25 +0200

    add new

--- src/redux/phonebook/phonebook-selectors.js
+++ src/redux/phonebook/phonebook-selectors.js
@@ -4,17 +4,11 @@ export const getContacts = (state) => state.contacts.items;
 export const getVisibleContacts = (state) => {
   const filter = getFilter(state);
   const contacts = getContacts(state);
+  console.log(state);
   const normalizedFilter = filter.toLowerCase();
 
   return contacts.filter((contact) => {
     console.log(contact.name);
     return contact.name.toLowerCase().includes(normalizedFilter);
-    // return contact.name.toLowerCase().includes(normalizedFilter);
   });
-
-  // return contacts.filter(
-  //   ({ name, number }) =>
-  //     name.name.toLowerCase().includes(filter.toLowerCase()) ||
-  //     number.includes(filter)
-  // );
 };
